48ae2c9c3bcf37d00dc1f219f8cf77853327c6b4
mohira <shukatsudaison@gmail.com>
2021-11-25 21:03:35 +0900

    p.45 借用の簡易モデルをベースにして、コードで実験する方法を理解した！
    invalidな状態遷移を含むコードを書くと、ルートによって違ったエラーメッセージがでるっぽいので、意識的な学習がしやすいと思う！
    
    コンパイラ任せもいけど、意識的な学習もあり！

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,35 @@ struct Foo {
     val: u32,
 }
 
-fn add_val(x: Foo, y: Foo) -> (u32, Foo, Foo) {
-    (x.val + y.val, x, y) // 1
-}
-
-fn mul_val(x: Foo, y: Foo) -> (u32, Foo, Foo) {
-    (x.val * y.val, x, y) // 2
-}
-
-
 fn main() {
-    let x = Foo { val: 3 };
-    let y = Foo { val: 6 };
+    let mut x = Foo { val: 10 }; // x は mutable変数
 
-    let (a, xn, yn) = add_val(x, y);
-    // a = add_val(x,y)
-    let (b, _, _) = mul_val(xn, yn);
+    {
+        let a = &mut x; // a は mutable参照
+        println!("a.val = {}", a.val);
 
-    println!("a = {}, b = {}", a, b);
+        // xは「&mut貸与中」のためエラー
+        // println!("x.val = {}", x.val);
+
+        let b: &Foo = a; // b は immutable参照
+        a.val = 20;  // a は 「&貸与中」状態のためエラー
+
+        println!("b.val = {}", b.val);
+        // ここで、bが借用している所有権が返却される
+
+        a.val = 30;
+    }
+
+    {
+        let c = &x; // cはimmutable参照
+        println!("c.val = {}", c.val);
+        println!("x.val = {}", x.val);
+
+        // let d = &mut x; // x は「&貸与中」状態のためのエラー
+        // d.val = 40;
+
+        println!("c.val = {}", c.val);
+    }
+
+    println!("x.val = {}", x.val);
 }
