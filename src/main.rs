e399f5ec601596a25ba7c6e3d57465b1ebb41cc6
mohira <shukatsudaison@gmail.com>
2021-11-25 21:35:02 +0900

    p.47 2.3.6 メソッド定義

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
-struct Foo {
+struct Vec2 {
+    x: f64,
+    y: f64,
+}
+
+struct MyU32 {
     val: u32,
+}
+// type MyInt u32
+
+impl Vec2 {
+    fn new(x: f64, y: f64) -> Self {
+        Vec2 { x, y }
+    }
+
+    fn norm(&self) -> f64 {
+        (self.x * self.x + self.y * self.y).sqrt()
+    }
+
+    fn set(&mut self, x: f64, y: f64) {
+        self.x = x;
+        self.y = y;
+    }
 }
 
 fn main() {
-    let mut x = Foo { val: 10 }; // x は mutable変数
+    let mut v = Vec2::new(10.0, 5.0);
 
+    println!("v.norm = {}", v.norm());
 
-    {
-        let c = &x;
-        println!("c.val = {}", c.val);
-        println!("x.val = {}", x.val);
-
-        let d = &mut x;
-        d.val = 40;
-        println!("d.val = {}", d.val);
-
-        println!("c.val = {}", c.val);
-    }
-
-    println!("x.val = {}", x.val);
+    v.set(3.8, 9.1);
+    println!("v.norm = {}", v.norm());
 }
