aa3e8b1711d59a1b8f896a32b8ed6502741526f8
mohira <shukatsudaison@gmail.com>
2021-11-25 21:30:30 +0900

    所有権とライフタイムの実験

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,15 @@ struct Foo {
 fn main() {
     let mut x = Foo { val: 10 }; // x は mutable変数
 
-    {
-        let a = &mut x; // a は mutable参照
-        println!("a.val = {}", a.val);
-
-        // xは「&mut貸与中」のためエラー
-        // println!("x.val = {}", x.val);
-
-        let b: &Foo = a; // b は immutable参照
-        a.val = 20;  // a は 「&貸与中」状態のためエラー
-
-        println!("b.val = {}", b.val);
-        // ここで、bが借用している所有権が返却される
-
-        a.val = 30;
-    }
 
     {
-        let c = &x; // cはimmutable参照
+        let c = &x;
         println!("c.val = {}", c.val);
         println!("x.val = {}", x.val);
 
-        // let d = &mut x; // x は「&貸与中」状態のためのエラー
-        // d.val = 40;
+        let d = &mut x;
+        d.val = 40;
+        println!("d.val = {}", d.val);
 
         println!("c.val = {}", c.val);
     }
