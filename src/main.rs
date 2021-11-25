5e3a4a1e010bb8b5682d8fcb6f59ef99d1277a39
mohira <shukatsudaison@gmail.com>
2021-11-25 20:52:57 +0900

    p.43

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+struct Foo {
+    val: u32,
+}
+
+fn add_val(x: Foo, y: Foo) -> (u32, Foo, Foo) {
+    (x.val + y.val, x, y) // 1
+}
+
+fn mul_val(x: Foo, y: Foo) -> (u32, Foo, Foo) {
+    (x.val * y.val, x, y) // 2
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let x = Foo { val: 3 };
+    let y = Foo { val: 6 };
+
+    let (a, xn, yn) = add_val(x, y);
+    // a = add_val(x,y)
+    let (b, _, _) = mul_val(xn, yn);
+
+    println!("a = {}, b = {}", a, b);
 }
