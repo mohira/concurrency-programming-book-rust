1d4b1265132d5100e855ac655fcb0e42ac6eeadf
mohira <shukatsudaison@gmail.com>
2022-04-14 18:29:46 +0900

    パン屋のアルゴリズム

--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,155 @@
-use std::sync::{Condvar, Mutex};
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::collections::LinkedList;
-use std::sync::{Arc, Condvar, Mutex};
+// 最適化抑制読み書き用
+use std::ptr::{read_volatile, write_volatile}; // ①
 
-// セマフォ用の型 1
-pub struct Semaphore{
-  mutex: Mutex<isize>,
-  cond: Condvar,
-  max: isize
+// メモリバリア用
+use std::sync::atomic::{fence, Ordering}; // ②
+use std::thread;
+
+const NUM_THREADS: usize = 4; // スレッド数
+const NUM_LOOP: usize = 1000000; // 各スレッドでのループ数
+
+// volatile用のマクロ ③
+macro_rules! read_mem {
+  ($addr: expr) => { unsafe {read_volatile($addr)}};
 }
 
-impl Semaphore {
-    pub fn new(max: isize) -> Self { // 2
-        Semaphore {
-          mutex: Mutex::new(0),
-          cond: Condvar::new(),
-          max,
+macro_rules! writer_mem {
+  ($addr: expr, $val: expr) => {
+      unsafe{write_volatile($addr, $val)}
+  };
+}
+
+// パン屋のアルゴリズム用の型 ④
+struct BakeryLock {
+  entering: [bool; NUM_THREADS],
+  tickets: [Option<u64>; NUM_THREADS],
+}
+
+impl BakeryLock {
+  // ロック関数。idxはスレッド番号
+
+  //スレッド idx がチケット取得中状態であることを示すために、
+  //entering[idx] を true に設定。また、その前後では、メモリバリアを行いアウトオブオーダ
+  // でのメモリ読み書きが行われることを防いでいる。
+  fn lock(&mut self, idx: usize) -> LockGuard {
+    // ここからチケット取得処理 ⑤
+    fence(Ordering::SeqCst); // ← これなんだっけ？
+    writer_mem!(&mut self.entering[idx], true);
+    fence(Ordering::SeqCst);
+
+    // 現在配布されているチケットの最大値を取得 ⑥
+    let mut max = 0;
+    for i in 0..NUM_THREADS {
+      if let Some(t) = read_mem!(&self.tickets[i]) {
+        max = max.max(t);
+      }
+    }
+
+    // 最大値+1 を自分のチケット番号とする ⑦
+    let ticket = max + 1;
+    writer_mem!(&mut self.tickets[idx], Some(ticket));
+
+    // もうチケットGETしたぜ！！！
+    fence(Ordering::SeqCst);
+    writer_mem!(&mut self.entering[idx], false); // ⑧
+    fence(Ordering::SeqCst);
+
+    // ここから待機処理 ⑨
+    for i in 0..NUM_THREADS {
+      // iはスレッド番号
+      if i == idx {
+        continue;
+      }
+
+      // スレッド i が、チケット取得中なら待機 ⑩
+      while read_mem!(&self.entering[i]) {}
+
+      loop {
+        // スレッドi と 自分の優先順位 を比較して
+        // 「自分の方が優先順位が高い」または「スレッドiが処理中でない」場合に待機を終了
+        // ⑪← 怪しい？
+
+        match read_mem!(&self.tickets[i]) {
+          // スレッドiのチケット番号を見る
+          Some(t) => {
+            // スレッドiのチケット番号より
+            // 「自分の番号が若い」
+            // または
+            // 「チケット番号が同じ かつ 自分のほうがスレッド番号が若い」
+            // 場合に待機終了
+
+            if ticket < t || (ticket == t && idx < i) {
+                break;
+              }
+          }
+          None => {
+            // スレッドiが処理中でない場合は待機終了
+            // 「チケット持ってない 」
+            // 「チケットを持っている」:=レジでの処理を待っている
+            break;
+          }
         }
-    }
-
-    pub fn wait(&self) {
-      // カウントが最大値以上なら待機 3
-      let mut cnt = self.mutex.lock().unwrap();
-
-      // 既にmax以上の数でプロセスが実行されている状態だったら
-      while *cnt >= self.max {
-          // アトミック
-          cnt = self.cond.wait(cnt).unwrap();
-      }
-
-      *cnt += 1;  // 4
-    }
-
-    pub fn post(&self) {
-      // カウントをデクリメント  4
-      let mut cnt = self.mutex.lock().unwrap();
-      *cnt -= 1;
-
-      // どれかのプロセスが終わる（＝ notify_one される）
-      if *cnt <= self.max {
-        self.cond.notify_one();
       }
     }
-}
-
-
-// 送信側のための型 1
-#[derive(Clone)]
-pub struct Sender<T> {
-  sem: Arc<Semaphore>, // 有限性を実現するセマフォ
-  buf: Arc<Mutex<LinkedList<T>>, // キュー
-  cond: Arc<Condvar>, // 読み込み側の条件変数
-}
-
-
-impl<T: Send> Sender<T> { // 2
-    // 送信関数
-    pub fn send(&self, data: T) {
-      self.sem.wait(); // キューの最大値に到達したら待機 3
-
-      let mut buf = self.buf.lock().unwrap();
-
-      buf.push_back(data); // エンキュー
-
-      self.cond.notify_one(); // 読み込み
-
-    }
-}
-
-
-pub struct Receiver<T> { // ①
-  sem: Arc<Semaphore>, // 有限性を実現するセマフォ
-  buf: Arc<Mutex<LinkedList<T>>>, // キュー
-  cond: Arc<Condvar>, // 読み込み側の条件変数
-}
-
-impl<T> Receiver<T> {
-  pub fn recv(&self) -> T {
-    let mut buf = self.buf.lock().unwrap();
-
-    loop {
-      // キューから取り出し ②
-      if let Some(data) = buf.pop_front() {
-        self.sem.post(); // ③
-        return data;
-      }
-
-      // 空の場合待機 ④
-      buf = self.cond.wait(buf).unwrap();
-    }
+    fence(Ordering::SeqCst);
+    LockGuard {idx}
   }
-    
+}
+
+// ロック管理用の型 ⑫
+struct LockGuard {
+  idx: usize
 }
 
 
+// Drop for isなに？
+impl Drop for LockGuard {
+  // ロック解放処理 ⑬
+  fn drop(&mut self) {
+    // スコープを外れたときにする挙動を定義
+    fence(Ordering::SeqCst);
+    writer_mem!(&mut LOCK.tickets[self.idx], None);
+  }
+}
 
+// グローバル変数⑭
+static mut LOCK: BakeryLock = BakeryLock {
+  entering: [false; NUM_THREADS],
+  tickets: [None; NUM_THREADS],
+};
 
-
-
-
-
-
+static mut COUNT: u64 = 0;
 
 
 fn main() {
+  // NUM_THREADSだけスレッドを生成
+  let mut v = Vec::new();
+  for i in 0..NUM_THREADS {
+    let th = thread::spawn(move || {
+      // NUM_LOOPだけループし、COUNTをインクリメント
+      for _ in 0..NUM_LOOP {
+        // ロック獲得     
+        let _lock = unsafe {LOCK.lock(i)};
 
+        unsafe {
+          let c = read_volatile(&COUNT);
+          write_volatile(&mut COUNT, c + 1);
+        }
+      }
+    });
+
+    v.push(th);
+  }
+
+  for th in v {
+    th.join().unwrap();
+
+
+    println!(
+      "COUNT={} (expected={})",
+      unsafe { COUNT },
+      NUM_LOOP * NUM_THREADS
+    )
+  }
 }
-
-
-
 
