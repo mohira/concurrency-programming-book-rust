cc2d1ad5a06ba769de500af73741f191d69fce0d
mohira <shukatsudaison@gmail.com>
2022-05-26 19:11:43 +0900

    いろいろ

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,19 @@
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, RwLock};
 use std::thread;
 
 
 fn main() {
-  // 箸が2本
-  let c0 = Arc::new(Mutex::new(()));
-  let c1 = Arc::new(Mutex::new(()));
+  let val = Arc::new(RwLock::new(true));
 
-  let c0_p0 = c0.clone();
-  let c1_p0 = c1.clone();
-
-  // 哲学者1
-  let p0 = thread::spawn(move || {
-      for _ in 0..10000 {
-        let _n1 = c0_p0.lock().unwrap();
-        let _n2 = c1_p0.lock().unwrap();
-        println!("0: eating");
-      }
-  });
-
-  // 哲学者2
-  let p1 = thread::spawn(move || {
-    for _ in 0..100000 {
-      let _n1 = c1.lock().unwrap();
-      let _n2 = c0.lock().unwrap();
-      println!("1: eating")
+  let t = thread::spawn(move || {
+    // let flag = val.read().unwrap();
+    let flag = *val.read().unwrap();
+    // if *flag {
+    if flag {
+        *val.write().unwrap() = false;
+      println!("flag is true")
     }
   });
 
-  p0.join().unwrap();
-  p1.join().unwrap();
-
+  t.join().unwrap();
 }
