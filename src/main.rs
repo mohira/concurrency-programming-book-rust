402b65d50a026f2efe5d0eb6232a5ec37cf9e0cd
mohira <shukatsudaison@gmail.com>
2022-06-23 20:46:50 +0900

    なおした

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // プロセスIDを表示
     println!("pid: {}", process::id());
 
-    let signals = Signals::new(&[SIGUSR1])?; // <2>
+    let mut signals = Signals::new(&[SIGUSR1])?; // <2>
     thread::spawn(move || {
         // シグナルを受信
         for sig in signals.forever() { // <3>
