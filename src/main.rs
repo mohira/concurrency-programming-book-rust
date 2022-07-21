fe55d471cccc15211a783875c31cf834c4119c87
mohira <shukatsudaison@gmail.com>
2022-07-21 20:04:15 +0900

    update cargo.toml for chapter5

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,4 @@
-// MEMO: consts::signal 追加
-use signal_hook::{iterator::Signals, consts::signal::SIGUSR1}; // <1>
-use std::{error::Error, process, thread, time::Duration};
 
-// https://crates.io/crates/signal-hook
-
-fn main() -> Result<(), Box<dyn Error>> {
-    // プロセスIDを表示
-    println!("pid: {}", process::id());
-
-    // MEMO: mutの追加
-    let mut signals = Signals::new(&[SIGUSR1])?; // <2>
-    thread::spawn(move || {
-        // シグナルを受信
-        for sig in signals.forever() { // <3>
-            println!("received signal: {:?}", sig);
-        }
-    });
-
-    // 10秒スリープ
-    thread::sleep(Duration::from_secs(10));
-    Ok(())
+fn main() {
+    print!("hello");
 }
